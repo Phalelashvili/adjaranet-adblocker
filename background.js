910753011b0eaea50e26db19fa5a5f166da74172
Phalelashvili <sphalelashvili@protonmail.com>
2019-10-13 11:50:21 +0400

    v1.1

---
+++ background.js
@@ -0,0 +1,14 @@
+﻿function blockRequest(details) {
+    return {cancel: true};
+}
+
+function updateFilters(urls) {
+    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+        chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+        
+    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: urls}, ['blocking']);    
+}
+
+fetch('https://steamboost.ge/ads.json').then(r => r.json()).then(response => {
+    updateFilters(response.ads)
+})
